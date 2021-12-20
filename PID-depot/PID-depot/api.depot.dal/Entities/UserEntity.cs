8e1c444ae87ea129d0a848f7bcc08aee93344282
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-20 14:05:53 +0100

    feat: Added user repo, base interface and userEntity. Added nuggetPackage DevhopTools

---
+++ PID-depot/PID-depot/api.depot.dal/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.depot.dal.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
---
+++ PID-depot/PID-depot/api.depot.dal/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using api.depot.dal.Entities;
+using api.depot.dal.IRepositories;
+using DevHopTools.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.depot.dal.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly Connection _connection;
+
+        public UserRepository(Connection connection)
+        {
+            _connection = connection ??
+                throw new ArgumentNullException(nameof(connection));
+        }
+
+        public Guid Create(UserEntity data)
+        {
+            Command command = new Command("spCreateUser", true);
+            command.AddParameter("email", data.Email);
+            command.AddParameter("password", data.Password);
+            command.AddParameter("firstname", data.Firstname);
+            command.AddParameter("lastname", data.Lastname);
+            command.AddParameter("birth_date", data.Birthdate);
+            command.AddParameter("registration_number", data.RegistrationNumber);
+
+            return Guid.Parse(_connection.ExecuteScalar(command).ToString());
+        }
+
+        public bool Delete(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GetById(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Guid key, UserEntity data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
