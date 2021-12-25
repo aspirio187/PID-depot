368f7df11b2a40d986c5db90652b533cf9bb2ff0
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-25 17:08:28 +0100

    feat: Created and implemented LessonTimetableRepository

---
+++ PID-depot/PID-depot/Api.Depot.DAL/Repositories/LessonTimetableRepository.cs
@@ -0,0 +1,70 @@
+﻿using Api.Depot.DAL.Entities;
+using Api.Depot.DAL.IRepositories;
+using Api.Depot.DAL.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.DAL.Repositories
+{
+    public class LessonTimetableRepository : ILessonTimetableRepository
+    {
+        private readonly Connection _connection;
+
+        public LessonTimetableRepository(Connection connection)
+        {
+            _connection = connection ??
+                throw new ArgumentNullException(nameof(connection));
+        }
+
+        public int Create(LessonTimetableEntity data)
+        {
+            if (data is null) throw new ArgumentNullException(nameof(data));
+
+            Command command = new Command("spCreateLessonTimetable", true);
+            command.AddParameter("lesson_id", data.LessonId);
+            command.AddParameter("starts_at", data.StartsAt);
+            command.AddParameter("ends_at", data.EndsAt);
+
+            return int.Parse(_connection.ExecuteScalar(command).ToString());
+        }
+
+        public bool Delete(int key)
+        {
+            Command command = new Command("spDeleteLessonTimetable", true);
+            command.AddParameter("timetable_id", key);
+
+            return _connection.ExecuteNonQuery(command) > 0;
+        }
+
+        public IEnumerable<LessonTimetableEntity> GetAll()
+        {
+            string query = "SELECT * FROM lesson_timetables";
+            Command command = new Command(query);
+
+            return _connection.ExecuteReader(command, lt => new LessonTimetableEntity(lt));
+        }
+
+        public LessonTimetableEntity GetById(int key)
+        {
+            string query = "SELECT * FROM lesson_timetables WHERE id = @id";
+            Command command = new Command(query);
+
+            return _connection.ExecuteReader(command, lt => new LessonTimetableEntity(lt)).FirstOrDefault();
+        }
+
+        public bool Update(int key, LessonTimetableEntity data)
+        {
+            if (data is null) throw new ArgumentNullException(nameof(data));
+
+            Command command = new Command("spUpdateLessonTimetable", true);
+            command.AddParameter("timetable_id", key);
+            command.AddParameter("starts_at", data.StartsAt);
+            command.AddParameter("ends_at", data.EndsAt);
+
+            return _connection.ExecuteNonQuery(command) > 0;
+        }
+    }
+}
