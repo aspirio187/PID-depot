c2ef815ccc1f19851459d2d631bce4899aa3760f
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 14:23:46 +0100

    feat: created and implemented lessonTimetable interface

---
+++ PID-depot/PID-depot/Api.Depot.BLL/IServices/ILessonTimetableService.cs
@@ -0,0 +1,20 @@
+﻿using Api.Depot.BLL.Dtos.LessonTimetableDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.IServices
+{
+    public interface ILessonTimetableService
+    {
+        IEnumerable<LessonTimetableDto> GetTimetables();
+        IEnumerable<LessonTimetableDto> GetLessonTimetables(int lessonId);
+        LessonTimetableDto GetTimetable(int id);
+        LessonTimetableDto GetLessonTimetable(int lessonId);
+        bool DeleteLessonTimetable(int id);
+        LessonTimetableDto CreateLessonTimetable(LessonTimetableCreationDto lessonTimetable);
+        LessonTimetableDto UpdateLessonTimetable(LessonTimetableDto lessonTimetable);
+    }
+}
