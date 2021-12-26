a2d8aa783b5fbe0e1bdfe69c6ab040d89ee59dcc
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 14:59:29 +0100

    feat: created lessonFileService interface

---
+++ PID-depot/PID-depot/Api.Depot.BLL/IServices/ILessonFileService.cs
@@ -0,0 +1,20 @@
+﻿using Api.Depot.BLL.Dtos.LessonFileDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.IServices
+{
+    public interface ILessonFileService
+    {
+        IEnumerable<LessonFileDto> GetFiles();
+        IEnumerable<LessonFileDto> GetLessonFiles(int lessonId);
+        IEnumerable<LessonFileDto> GetLessonDetailFiles(int lessonDetailId);
+        LessonFileDto GetFile(int id);
+        bool DeleteLessonFile(int id);
+        LessonFileDto CreateLessonFile(LessonFileCreationDto lessonFile);
+        LessonFileDto UpdateLessonFile(LessonFileDto lessonFile);
+    }
+}
