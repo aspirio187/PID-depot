46e82217b111b4c57f5c74b69e44d64bd7f7ddf4
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 18:24:02 +0100

    feat: Add Authentication controller and Register form

---
+++ PID-depot/PID-depot/Api.Depot.UIL/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Api.Depot.BLL.IServices;
+using Api.Depot.UIL.Models.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Api.Depot.UIL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService ??
+                throw new ArgumentNullException(nameof(userService));
+        }
+
+        [HttpGet]
+        [Route("Register")]
+        public IActionResult Register([FromBody] RegisterForm register)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+            // TODO : Vérifier si l'email est disponible
+        }
+    }
+}
---
+++ PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/RegisterForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Depot.UIL.Models.Forms
+{
+    public class RegisterForm
+    {
+        [Required(AllowEmptyStrings = false)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [RegularExpression("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_+]).{8,20}$")]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(50)]
+        public string Firstname { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [MaxLength(100)]
+        public string Lastname { get; set; }
+
+        [Required]
+        public DateTime Birthdate { get; set; }
+    }
+}
