18975291602ac0c29e2122fa58591038a4da82d5
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 14:17:18 +0100

    feat: created and implemented LessonService with interface

---
+++ PID-depot/PID-depot/Api.Depot.BLL/IServices/ILessonService.cs
@@ -0,0 +1,18 @@
+﻿using Api.Depot.BLL.Dtos.LessonDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.IServices
+{
+    public interface ILessonService
+    {
+        IEnumerable<LessonDto> GetLessons();
+        LessonDto GetLesson(int id);
+        bool DeleteLesson(int id);
+        LessonDto CreateLesson(LessonCreationDto lesson);
+        LessonDto UpdateLesson(LessonDto lesson);
+    }
+}
---
+++ PID-depot/PID-depot/Api.Depot.BLL/Services/LessonService.cs
@@ -0,0 +1,49 @@
+﻿using Api.Depot.BLL.Dtos.LessonDtos;
+using Api.Depot.BLL.IServices;
+using Api.Depot.DAL.Entities;
+using Api.Depot.DAL.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Services
+{
+    public class LessonService : ILessonService
+    {
+        private readonly ILessonRepository _lessonRepository;
+
+        public LessonDto CreateLesson(LessonCreationDto lesson)
+        {
+            if (lesson is null) throw new ArgumentNullException(nameof(lesson));
+
+            LessonEntity lessonToCreate = lesson.MapDAL();
+            int createdLessonId = _lessonRepository.Create(lessonToCreate);
+            return new LessonDto(_lessonRepository.GetById(createdLessonId));
+        }
+
+        public bool DeleteLesson(int id)
+        {
+            return _lessonRepository.Delete(id);
+        }
+
+        public LessonDto GetLesson(int id)
+        {
+            return new LessonDto(_lessonRepository.GetById(id));
+        }
+
+        public IEnumerable<LessonDto> GetLessons()
+        {
+            return _lessonRepository.GetAll().Select(l => new LessonDto(l));
+        }
+
+        public LessonDto UpdateLesson(LessonDto lesson)
+        {
+            if (lesson is null) throw new ArgumentNullException(nameof(lesson));
+
+            LessonEntity lessonToUpdate = lesson.MapDAL();
+            return _lessonRepository.Update(lesson.Id, lessonToUpdate) ? lesson : null;
+        }
+    }
+}
