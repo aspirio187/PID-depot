514505909f4e797c2f8fc21095fd21a635b96323
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 14:09:29 +0100

    feat: Created Lesson dtos

---
+++ PID-depot/PID-depot/Api.Depot.BLL/Dtos/LessonDtos/LessonCreationDto.cs
@@ -0,0 +1,24 @@
+﻿using Api.Depot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Dtos.LessonDtos
+{
+    public class LessonCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public LessonEntity MapDAL()
+        {
+            return new LessonEntity()
+            {
+                Name = Name,
+                Description = Description,
+            };
+        }
+    }
+}
---
+++ PID-depot/PID-depot/Api.Depot.BLL/Dtos/LessonDtos/LessonDto.cs
@@ -0,0 +1,38 @@
+﻿using Api.Depot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Dtos.LessonDtos
+{
+    public class LessonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public LessonDto()
+        {
+
+        }
+
+        public LessonDto(LessonEntity lesson)
+        {
+            Id = lesson.Id;
+            Name = lesson.Name;
+            Description = lesson.Description;
+        }
+
+        public LessonEntity MapDAL()
+        {
+            return new LessonEntity()
+            {
+                Id = Id,
+                Name = Name,
+                Description = Description
+            };
+        }
+    }
+}
