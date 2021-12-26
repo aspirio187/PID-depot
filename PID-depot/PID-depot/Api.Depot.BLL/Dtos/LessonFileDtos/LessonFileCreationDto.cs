5c65f864eb53143605909eb3df0b75a6ebb3cd10
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-26 14:56:48 +0100

    feat: Created lessonFile dtos

---
+++ PID-depot/PID-depot/Api.Depot.BLL/Dtos/LessonFileDtos/LessonFileCreationDto.cs
@@ -0,0 +1,24 @@
+﻿using Api.Depot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Dtos.LessonFileDtos
+{
+    public class LessonFileCreationDto
+    {
+        public string FilePath { get; set; }
+        public int LessonDetailId { get; set; }
+
+        public static implicit operator LessonFileEntity(LessonFileCreationDto lessonFile)
+        {
+            return new LessonFileEntity()
+            {
+                FilePath = lessonFile.FilePath,
+                LessonDetailId = lessonFile.LessonDetailId,
+            };
+        }
+    }
+}
---
+++ PID-depot/PID-depot/Api.Depot.BLL/Dtos/LessonFileDtos/LessonFileDto.cs
@@ -0,0 +1,43 @@
+﻿using Api.Depot.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Dtos.LessonFileDtos
+{
+    public class LessonFileDto
+    {
+        public int Id { get; set; }
+        public string FilePath { get; set; }
+        public int LessonDetailId { get; set; }
+
+        public LessonFileDto()
+        {
+
+        }
+
+        public LessonFileDto(LessonFileEntity lessonFile)
+        {
+            Id = lessonFile.Id;
+            FilePath = lessonFile.FilePath;
+            LessonDetailId = lessonFile.LessonDetailId;
+        }
+
+        public static implicit operator LessonFileDto(LessonFileEntity lessonFile)
+        {
+            return new LessonFileDto(lessonFile);
+        }
+
+        public static implicit operator LessonFileEntity(LessonFileDto lessonFile)
+        {
+            return new LessonFileEntity()
+            {
+                Id = lessonFile.Id,
+                FilePath = lessonFile.FilePath,
+                LessonDetailId = lessonFile.LessonDetailId
+            };
+        }
+    }
+}
