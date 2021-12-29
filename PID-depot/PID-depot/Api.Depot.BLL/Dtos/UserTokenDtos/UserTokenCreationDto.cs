e7cfac32c9df5c48a5568f759e19d01a4b98f718
Soultan Hatsijev <soultan_98@hotmail.com>
2021-12-29 16:52:17 +0100

    feat: created UserToken dtos

---
+++ PID-depot/PID-depot/Api.Depot.BLL/Dtos/UserTokenDtos/UserTokenCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Depot.BLL.Dtos.UserTokenDtos
+{
+    public class UserTokenCreationDto
+    {
+        public UserTokenType TokenType { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
