e2a0d2f21683e797146a6b0117114e6ff81443ea
Aspirio <soultan_98@hotmail.com>
2022-01-31 11:54:36 +0100

    feat: Created IsSignedIn property

--- PID-depot/PID-depot/depot.uil/Managers/AuthenticationManager.cs
+++ PID-depot/PID-depot/depot.uil/Managers/AuthenticationManager.cs
@@ -1,8 +1,21 @@
 ﻿
+using Depot.UIL.Models;
+
 namespace Depot.UIL.Managers
 {
     public class AuthenticationManager
     {
+        public UserModel User { get; private set; }
+
+        public bool IsSignedIn
+        {
+            get
+            {
+                // TODO : Vérifier s'il existe un token jwt
+                return false;
+            }
+        }
+
         public AuthenticationManager()
         {
 
