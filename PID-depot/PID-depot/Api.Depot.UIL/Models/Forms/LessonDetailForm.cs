db17721d53d64d6d9d4631a7f845a820fc1d5f0a
Aspirio <soultan_98@hotmail.com>
2022-02-01 12:06:54 +0100

    feat: created LessonDetail Form

---
+++ PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/LessonDetailForm.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Depot.UIL.Models.Forms
+{
+    public class LessonDetailForm
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public int LessonTimetableId { get; set; }
+    }
+}
