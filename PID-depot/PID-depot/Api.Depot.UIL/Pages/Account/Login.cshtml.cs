0f91f94f07c914f3999a788c4bdc2a0bbb7182b1
Aspirio <soultan_98@hotmail.com>
2022-02-09 09:48:19 +0100

    feat: Created Login page

--- PID-depot/PID-depot/Api.Depot.UIL/Pages/Account/Login.cshtml.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,25 @@
+using Api.Depot.UIL.Models.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace Api.Depot.UIL.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public LoginForm Login { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUl = null)
         {
+            if (returnUl is not null) ReturnUrl = returnUl;
+        }
+
+        public async Task OnPostAsync()
+        {
+
         }
     }
 }
