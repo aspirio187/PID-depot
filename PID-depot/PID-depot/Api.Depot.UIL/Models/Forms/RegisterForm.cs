3ea04ab7dd7fce675f9756a7fe79608b6c593dfd
Aspirio <soultan_98@hotmail.com>
2022-03-15 11:30:04 +0100

    feat: Created Register page and implemented error messages for RegisterForm validation attributes

--- PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/RegisterForm.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/RegisterForm.cs
@@ -5,27 +5,28 @@ namespace Api.Depot.UIL.Models.Forms
 {
     public class RegisterForm
     {
-        [Required(AllowEmptyStrings = false)]
-        [EmailAddress]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "L'adresse email est requise!")]
+        [EmailAddress(ErrorMessage = "Le format de l'adresse email est incorrect!")]
         public string Email { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [RegularExpression("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_+]).{8,20}$")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez choisir un mot de passe!")]
+        [RegularExpression("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-_+]).{8,20}$",
+            ErrorMessage = "Le mot de passe doit faire entre 8 et 20 caractères et doit contenir au moins une minuscule, une majuscule, un nombre et un caractère spécial!")]
         public string Password { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [Compare("Password")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez confirmer le mot de passe!")]
+        [Compare("Password", ErrorMessage = "Les mots de passes ne concordent pas!")]
         public string ConfirmPassword { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [MaxLength(50)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer votre prénom!")]
+        [MaxLength(50, ErrorMessage = "Le prénom ne peut pas faire plus de 50 caractères!")]
         public string Firstname { get; set; }
 
-        [Required(AllowEmptyStrings = false)]
-        [MaxLength(100)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer votre nom de famille!")]
+        [MaxLength(100, ErrorMessage = "Le nom de famille ne peut pas faire plus de 100 caractères!")]
         public string Lastname { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Veuillez entrer votre date de naissance!")]
         public DateTime Birthdate { get; set; }
     }
 }
---
+++ PID-depot/PID-depot/Api.Depot.UIL/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,33 @@
+using Api.Depot.UIL.Managers;
+using Api.Depot.UIL.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Api.Depot.UIL.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ILogger _logger;
+        private readonly IAuthManager _authManager;
+
+        public RegisterModel(ILogger logger, IAuthManager authManager)
+        {
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+            _authManager = authManager ??
+                throw new ArgumentNullException(nameof(authManager));
+        }
+
+        [BindProperty]
+        public RegisterForm RegForm { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public void OnGet(string returnUl = null)
+        {
+            if (returnUl is not null) ReturnUrl = returnUl;
+        }
+    }
+}
