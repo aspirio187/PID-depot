8dff2dbcb184c7edb7c4a5f9dc48cccd2b9a961f
Aspirio <soultan_98@hotmail.com>
2022-03-21 14:41:20 +0100

    feat: implemented Lesson creation for a teacher

--- PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/CreateLesson.cshtml.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/CreateLesson.cshtml.cs
@@ -1,5 +1,9 @@
+using Api.Depot.BLL.Dtos.LessonDtos;
 using Api.Depot.BLL.IServices;
 using Api.Depot.UIL.Models.Forms;
+using Api.Depot.UIL.Static_Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -9,6 +13,7 @@ using System.Security.Claims;
 
 namespace Api.Depot.UIL.Areas.Teachers.Pages
 {
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = RolesData.AUTH_TEACHER_ROLE)]
     public class CreateLessonModel : PageModel
     {
         private readonly ILogger _logger;
@@ -31,6 +36,33 @@ namespace Api.Depot.UIL.Areas.Teachers.Pages
             {
                 UserId = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value)
             };
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (User.IsInRole(RolesData.TEACHER_ROLE) || User.IsInRole(RolesData.ADMIN_ROLE))
+                    {
+                        LessonDto createdLesson = _lessonService.CreateLesson(Lesson.MapToBLL());
+                        if (createdLesson is null)
+                        {
+                            ModelState.AddModelError("Lesson creation", "La création du cours a échoué");
+                            return Page();
+                        }
+
+                        RedirectToPage("/Index", new { area = "Teachers" });
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message);
+                    return Page();
+                }
+            }
+            return Page();
         }
     }
 }
--- PID-depot/PID-depot/Api.Depot.UIL/Static Data/RolesData.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Static Data/RolesData.cs
@@ -6,5 +6,8 @@
         public const string TEACHER_ROLE = "Teacher";
         public const string USER_ROLE = "User";
         public const string ADMIN_ROLE = "Admin";
+
+        public const string AUTH_TEACHER_ROLE = "Admin,Teacher";
+        public const string AUTH_STUDENT_ROLE = "Admin,Student";
     }
 }
