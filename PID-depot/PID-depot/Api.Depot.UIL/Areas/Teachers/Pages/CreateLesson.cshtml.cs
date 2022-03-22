287d95b1aaf7765e3e6d7aa441c1d8d0b1bc4c50
Aspirio <soultan_98@hotmail.com>
2022-03-22 12:47:28 +0100

    feat: added validation and destruction steps
    If one of the creation step fails then all preceding creation should be deleted to avoid any parasitical values in the database

--- PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/CreateLesson.cshtml.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/CreateLesson.cshtml.cs
@@ -53,14 +53,16 @@ namespace Api.Depot.UIL.Areas.Teachers.Pages
                         LessonDto createdLesson = _lessonService.CreateLesson(Lesson.MapToBLL());
                         if (createdLesson is null)
                         {
+                            // If the lesson couldn't be created, just returning a ModelState error
                             ModelState.AddModelError("Lesson creation", "La création du cours a échoué");
                             return Page();
                         }
 
                         if (!_lessonService.AddLessonUser(createdLesson.Id, Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier))))
                         {
+                            // If the user couldn't be added to the lesson we delete the lesson. If the lesson could not be deleted we log an error
                             ModelState.AddModelError("Add user", "Vous n'avez pas pu être ajouté comme professeur pour ce cours");
-                            if(_lessonService.DeleteLesson(createdLesson.Id))
+                            if (!_lessonService.DeleteLesson(createdLesson.Id))
                             {
                                 _logger.LogError("Couldn't delete lesson with id : {0}", createdLesson.Id);
                             }
@@ -70,7 +72,23 @@ namespace Api.Depot.UIL.Areas.Teachers.Pages
                         LessonTimetableDto createdLessonTimetable = _lessonTimetableService.CreateLessonTimetable(Lesson.MapToBLL(createdLesson.Id));
                         if (createdLessonTimetable is null)
                         {
+                            // If the timetable couldn't be created and linked to the lesson we delete the user / lesson relation then we delete
+                            // the lessson. If one of these steps didn't go successfully through, we log errors
                             ModelState.AddModelError("Timetable creation", "L'horaire crée n'a pas pu être ajouté au cours");
+
+                            if (!_lessonService.DeleteLessonUser(createdLesson.Id, Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier))))
+                            {
+                                _logger.LogError("Couldn't delete lesson's user for lesson : {0} with user id : {1}",
+                                    createdLesson.Id,
+                                    Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)));
+                            }
+                            else
+                            {
+                                if (!_lessonService.DeleteLesson(createdLesson.Id))
+                                {
+                                    _logger.LogError("Couldn't delete lesson with id : {0}", createdLesson.Id);
+                                }
+                            }
 
                             return Page();
                         }
