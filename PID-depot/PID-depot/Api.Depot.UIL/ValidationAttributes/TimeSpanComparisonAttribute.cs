637738b04e55d027af1d867d5d7f48c137cfa693
Aspirio <soultan_98@hotmail.com>
2022-03-23 14:41:27 +0100

    refactor: Added verification on IsSelected if it is declared to avoid useless error messages

--- PID-depot/PID-depot/Api.Depot.UIL/ValidationAttributes/TimeSpanComparisonAttribute.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/ValidationAttributes/TimeSpanComparisonAttribute.cs
@@ -7,20 +7,40 @@ namespace Api.Depot.UIL.ValidationAttributes
     public class TimeSpanComparisonAttribute : ValidationAttribute
     {
         public string MinimumTimeSpanPropertyName { get; set; }
+        public string IsSelectedPropertyName { get; set; }
 
-        public TimeSpanComparisonAttribute(string minimumTimeSpanPropertyName)
+        public TimeSpanComparisonAttribute(string minimumTimeSpanPropertyName, string isSelectedPropertyName = null)
         {
             minimumTimeSpanPropertyName = minimumTimeSpanPropertyName.Trim();
+
             if (minimumTimeSpanPropertyName is null)
                 throw new ArgumentNullException(nameof(minimumTimeSpanPropertyName));
             if (minimumTimeSpanPropertyName.Length == 0)
                 throw new ArgumentException($"{nameof(minimumTimeSpanPropertyName)} cannot be empty string!");
 
+            if (isSelectedPropertyName is not null)
+            {
+                isSelectedPropertyName = isSelectedPropertyName.Trim();
+                if (isSelectedPropertyName.Length == 0)
+                    throw new ArgumentException($"{nameof(isSelectedPropertyName)} cannot be empty string!");
+            }
+
+            IsSelectedPropertyName = isSelectedPropertyName;
             MinimumTimeSpanPropertyName = minimumTimeSpanPropertyName;
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            if (IsSelectedPropertyName is not null)
+            {
+                PropertyInfo isSelectedPropertyInfo = validationContext.ObjectType.GetProperty(IsSelectedPropertyName);
+                if (isSelectedPropertyInfo is null) throw new NullReferenceException($"{nameof(IsSelectedPropertyName)} doesn't exist!");
+
+                bool isSelected = (bool)isSelectedPropertyInfo.GetValue(value, null);
+
+                if (isSelected == false) return ValidationResult.Success;
+            }
+
             PropertyInfo basePropertyInfo = validationContext.ObjectType.GetProperty(MinimumTimeSpanPropertyName);
             if (basePropertyInfo is null) throw new NullReferenceException($"{nameof(MinimumTimeSpanPropertyName)} doesn't exist!");
 
