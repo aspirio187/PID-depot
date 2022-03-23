af0a813663210afe31bb56e03d996d4698f8348d
Aspirio <soultan_98@hotmail.com>
2022-03-23 14:25:17 +0100

    feat: Implemented TimeSpanDelimitation attribute

---
+++ PID-depot/PID-depot/Api.Depot.UIL/ValidationAttributes/TimeSpanDelimitationAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Depot.UIL.ValidationAttributes
+{
+    public class TimeSpanDelimitationAttribute : ValidationAttribute
+    {
+        public int MinimumHours { get; set; }
+        public int MinimumMinutes { get; set; }
+        public int MaximumHours { get; set; }
+        public int MaximumMinutes { get; set; }
+
+        public TimeSpanDelimitationAttribute(int minimumHours, int minimumMinutes, int maximumHours, int maximumMinutes)
+        {
+            MinimumHours = minimumHours;
+            MinimumMinutes = minimumMinutes;
+            MaximumHours = maximumHours;
+            MaximumMinutes = maximumMinutes;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            TimeSpan actualTimeSpan = (TimeSpan)value;
+            TimeSpan minimumTimeSpan = new TimeSpan(MinimumHours, MinimumMinutes, 0);
+            TimeSpan maximumTimeSpan = new TimeSpan(MaximumHours, MaximumMinutes, 0);
+
+            if (actualTimeSpan < minimumTimeSpan || actualTimeSpan > maximumTimeSpan)
+                return new ValidationResult(base.ErrorMessage);
+
+            return ValidationResult.Success;
+        }
+    }
+}
