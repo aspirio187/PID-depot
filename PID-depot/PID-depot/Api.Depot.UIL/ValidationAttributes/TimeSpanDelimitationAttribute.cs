2d5117ce4ce75f10c1e89e8a91918f5ed7d44702
Aspirio <soultan_98@hotmail.com>
2022-03-23 14:47:37 +0100

    refactor: added verification on optional isSelected property to avoid useless errors

--- PID-depot/PID-depot/Api.Depot.UIL/ValidationAttributes/TimeSpanDelimitationAttribute.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/ValidationAttributes/TimeSpanDelimitationAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace Api.Depot.UIL.ValidationAttributes
 {
@@ -9,9 +10,28 @@ namespace Api.Depot.UIL.ValidationAttributes
         public int MinimumMinutes { get; set; }
         public int MaximumHours { get; set; }
         public int MaximumMinutes { get; set; }
+        public string IsSelectedPropertyName { get; set; }
 
-        public TimeSpanDelimitationAttribute(int minimumHours, int minimumMinutes, int maximumHours, int maximumMinutes)
+        public TimeSpanDelimitationAttribute(int minimumHours, int minimumMinutes, int maximumHours, int maximumMinutes,
+            string isSelectedPropertyName = null)
         {
+            if (minimumHours < 0 || minimumHours > 24)
+                throw new ArgumentOutOfRangeException($"{nameof(minimumHours)} must be greater than or equals to 0 or smaller than 24");
+            if (minimumMinutes < 0 || minimumMinutes > 60)
+                throw new ArgumentOutOfRangeException($"{nameof(minimumMinutes)} must be greater than or equals to 0 or smaller than 60");
+            if (maximumHours < 0 || maximumHours > 24)
+                throw new ArgumentOutOfRangeException($"{nameof(maximumHours)} must be greater than or equals to 0 or smaller than 24");
+            if (maximumMinutes < 0 || maximumMinutes > 60)
+                throw new ArgumentOutOfRangeException($"{nameof(maximumMinutes)} must be greater than or equals to 0 or smaller than 60");
+
+            if (isSelectedPropertyName is not null)
+            {
+                isSelectedPropertyName = isSelectedPropertyName.Trim();
+                if (isSelectedPropertyName.Length == 0)
+                    throw new ArgumentException($"{nameof(isSelectedPropertyName)} cannot be empty string!");
+            }
+
+            IsSelectedPropertyName = isSelectedPropertyName;
             MinimumHours = minimumHours;
             MinimumMinutes = minimumMinutes;
             MaximumHours = maximumHours;
@@ -20,6 +40,16 @@ namespace Api.Depot.UIL.ValidationAttributes
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
+            if (IsSelectedPropertyName is not null)
+            {
+                PropertyInfo isSelectedPropertyInfo = validationContext.ObjectType.GetProperty(IsSelectedPropertyName);
+                if (isSelectedPropertyInfo is null) throw new NullReferenceException($"{nameof(IsSelectedPropertyName)} doesn't exist!");
+
+                bool isSelected = (bool)isSelectedPropertyInfo.GetValue(value, null);
+
+                if (isSelected == false) return ValidationResult.Success;
+            }
+
             TimeSpan actualTimeSpan = (TimeSpan)value;
             TimeSpan minimumTimeSpan = new TimeSpan(MinimumHours, MinimumMinutes, 0);
             TimeSpan maximumTimeSpan = new TimeSpan(MaximumHours, MaximumMinutes, 0);
