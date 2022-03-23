a1281c1a132c7737742ad06d2d4dabcea67c1144
Aspirio <soultan_98@hotmail.com>
2022-03-23 14:28:22 +0100

    feat: added timespan comparison and delimitation attributes to properties

--- PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/LessonDayForm.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Models/Forms/LessonDayForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Api.Depot.UIL.ValidationAttributes;
+using System;
 
 namespace Api.Depot.UIL.Models.Forms
 {
@@ -9,7 +10,14 @@ namespace Api.Depot.UIL.Models.Forms
         public bool IsSelected { get; set; }
         // TODO : Ajouter un attribut qui vérifie si l'heure de début est plus petite que l'heure de fin
         // TODO : Ajouter un attribut qui empêche les heures de début et de fin d'être en dessous de 8h ou 18h
+        [TimeSpanDelimitation(8, 0, 18, 0,
+            ErrorMessage = "Les cours ne peuvent commencer qu'entre 8:00 et 18:00 compris !")]
         public TimeSpan StartsAt { get; set; }
+
+        [TimeSpanDelimitation(8, 0, 18, 0,
+            ErrorMessage = "Les cours ne peuvent se terminer qu'entre 8:00 et 18:00 compris !")]
+        [TimeSpanComparison(nameof(StartsAt),
+            ErrorMessage = "La fin du cours ne peut pas précéder ou égaler le début du cours !")]
         public TimeSpan EndsAt { get; set; }
     }
 }
