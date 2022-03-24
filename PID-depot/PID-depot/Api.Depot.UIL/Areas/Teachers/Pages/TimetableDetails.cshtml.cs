daf6db763300c9775cd230003b8f2b8eaa6cc31f
Aspirio <soultan_98@hotmail.com>
2022-03-24 13:27:02 +0100

    feat: create Timetable details page

---
+++ PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/TimetableDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Api.Depot.UIL.Areas.Teachers.Pages
+{
+    public class TimetableDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
