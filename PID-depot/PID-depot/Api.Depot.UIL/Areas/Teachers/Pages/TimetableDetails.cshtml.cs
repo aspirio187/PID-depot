1e2aa9fd22971b4bd014a9aba3af55602e969726
Aspirio <soultan_98@hotmail.com>
2022-03-24 14:02:43 +0100

    feat/fix/refactor: Fixed the redirection url in Schedule. Refactored the input closing tag in CreateLesson and the button style in TimetableDetails. Tester directory path value

--- PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/TimetableDetails.cshtml.cs
+++ PID-depot/PID-depot/Api.Depot.UIL/Areas/Teachers/Pages/TimetableDetails.cshtml.cs
@@ -1,11 +1,14 @@
+using Api.Depot.BLL.Dtos.LessonDetailDtos;
 using Api.Depot.BLL.IServices;
 using Api.Depot.UIL.Models.Forms;
+using Api.Depot.UIL.Static_Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Api.Depot.UIL.Areas.Teachers.Pages
 {
@@ -30,22 +33,36 @@ namespace Api.Depot.UIL.Areas.Teachers.Pages
 
         public List<LessonFileForm> LessonFile { get; set; }
 
-        public void OnGet(int id)
+        public IActionResult OnGet(int id)
         {
-            if (id != 0)
+            if (id == 0) return RedirectToPage("Index", new { Area = "Teachers" });
+
+            LessonDetail = new LessonDetailForm()
             {
-                LessonDetail = new LessonDetailForm()
-                {
-                    LessonTimetableId = id,
-                };
-            }
+                LessonTimetableId = id,
+            };
+
+            return Page();
         }
 
         public IActionResult OnPost(List<IFormFile> postedFiles)
         {
             if (ModelState.IsValid)
             {
+                //LessonDetailDto createdLessonDetails = _lessonDetailService.CreateLessonDetail(LessonDetail.MapToBLL());
+                //if (createdLessonDetails is null)
+                //{
+                //    ModelState.AddModelError("Lesson Details Creation", "La création des détails du cours a echouée");
+                //    _logger.LogError("Lesson details creation failed");
+                //    return Page();
+                //}
 
+                var v = Path.GetFullPath(FilesData.FILE_DIRECTORY_PATH);
+
+                foreach (var file in postedFiles)
+                {
+
+                }
             }
 
             return Page();
---
+++ PID-depot/PID-depot/Api.Depot.UIL/Static Data/FilesData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace Api.Depot.UIL.Static_Data
+{
+    public static class FilesData
+    {
+        public const string FILE_DIRECTORY_PATH = "Files";
+    }
+}
