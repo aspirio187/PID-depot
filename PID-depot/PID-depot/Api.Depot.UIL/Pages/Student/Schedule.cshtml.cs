37a87e6b44f60b3d876a431a854869115b7625f8
Aspirio <soultan_98@hotmail.com>
2022-03-25 11:18:50 +0100

    feat: created shedule page for students

---
+++ PID-depot/PID-depot/Api.Depot.UIL/Pages/Student/Schedule.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Api.Depot.UIL.Pages.Student
+{
+    public class ScheduleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
